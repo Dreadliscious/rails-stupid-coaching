96dc516421822854182e29987fde597fb7a2e3e0
damien desparain <damien.cpielerins@gmail.com>
2022-05-30 14:40:42 +0200

    stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,15 @@
 class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    toto = params[:answer]
+    if toto == "I am going to work"
+      @answer = "great"
+    elsif toto.chars.last == '?'
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
